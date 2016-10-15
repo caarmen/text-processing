4708574239d7c92608402e55a6bdfb007b1a9496
Carmen Alvarez <c@rmen.ca>
2016-10-15 19:24:30 +0200

    Fix some code insepction issues in find_frequencies.py

--- find_frequencies.py
+++ find_frequencies.py
@@ -3,32 +3,32 @@ import os
 import sys
 import fileinput
 
-def main(argv):
+
+def main():
     if sys.stdin.isatty():
         usage()
 
     here = os.path.dirname(os.path.realpath(__file__))
-    dbpath = os.path.join(here, "google-english-1gram-frequencies-2008.txt.gz")
-    frequency_db = {}
-    with gzip.open(dbpath, "rb") as f:
+    frequency_data_path = os.path.join(here, "google-english-1gram-frequencies-2008.txt.gz")
+    frequency_dict = {}
+    with gzip.open(frequency_data_path, "rb") as f:
         for line in f:
             (word, frequency) = line.strip().split()
-            frequency_db[word] = frequency
+            frequency_dict[word] = frequency
 
     for line in fileinput.input():
         word = line.strip()
-        if word in frequency_db:
-            frequency = frequency_db[word]
-            print("%s\t%s") % (word, frequency)
+        if word in frequency_dict:
+            print("%s\t%s") % (word, frequency_dict[word])
         else:
-            print >> sys.stderr, ("%s not found") % (word)
+            sys.stderr.write("%s not found\n" % word)
 
 
 def usage():
-    print ("This script reads a list of words from stdin and prints the frequency of each word to stdout.")
-    print
-    print ("Usage: cat /path/to/wordlist.txt | python %s") % (__file__)
+    print("This script reads a list of words from stdin and prints the frequency of each word to stdout.\n")
+    print("Usage: cat /path/to/word_list.txt | python %s") % __file__
     exit(1)
 
+
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
